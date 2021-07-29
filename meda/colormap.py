c91e7297d364da441493412a6d5ccf515bd19ee4
perillaroc <perillaroc@gmail.com>
2021-07-29 10:31:19 +0800

    :sparkles: support index when get ncl colormap.

--- meda/colormap.py
+++ meda/colormap.py
@@ -1,13 +1,42 @@
 import pkg_resources
 import re
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
 
 import numpy as np
 import matplotlib.colors as mcolors
 
 
-def get_ncl_colormap(name) -> Optional[mcolors.ListedColormap]:
+def get_ncl_colormap(name, index: Optional[np.ndarray] = None, face_color="white") -> Optional[mcolors.ListedColormap]:
+    """
+    Generate Matplotlib colormap from NCL color map files in resource directory.
+
+    Parameters
+    ----------
+    name
+        ncl color map name without extension.
+    index
+    face_color
+
+    Returns
+    -------
+    matplotlib.colors.ListedColormap
+    """
+    raw_colormap = _get_raw_ncl_colormap(name)
+    if raw_colormap is None or index is None:
+        return raw_colormap
+
+    colors = []
+    if index[0] == -1:
+        colors = [face_color]
+        index = index[1:]
+    raw_colors = raw_colormap(index)
+    colors.extend(raw_colors)
+    color_map = mcolors.ListedColormap(colors)
+    return color_map
+
+
+def _get_raw_ncl_colormap(name) -> mcolors.ListedColormap:
     """
     Generate Matplotlib colormap from NCL color map files in resource directory.
 
