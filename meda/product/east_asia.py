5d5b7fbe934466592b5ea98c6e3b584f6afad4b5
perillaroc <perillaroc@gmail.com>
2021-08-31 22:26:27 +0800

    :art: add parameters for generate_east_asia_plot
    :bulb: update docstrings.

--- meda/product/east_asia.py
+++ meda/product/east_asia.py
@@ -1,8 +1,9 @@
+from typing import Tuple
+
 import numpy as np
-
 import cartopy.crs as ccrs
-
 import matplotlib.pyplot as plt
+import matplotlib.axes
 
 from meda.map import (
     get_china_map,
@@ -20,22 +21,34 @@ from meda.util import (
 )
 
 
-def generate_east_asia_plot():
+def generate_east_asia_plot(
+        figure_width=6,
+        figure_height=6,
+        figure_dpi=400
+) -> Tuple[matplotlib.axes.Axes, matplotlib.axes.Axes]:
     """
     生成东亚区域图片底图
 
+    Parameters
+    ----------
+    figure_width
+    figure_height
+    figure_dpi
+        分辨率，`figure_height` * `figure_width` = 图片宽像素点数
+
     Returns
     -------
-
+    Tuple[matplotlib.axes.Axes, matplotlib.axes.Axes]
+        主地图和子图
     """
     projection = ccrs.PlateCarree()
     cn_features = get_china_map()
     nine_features = get_china_nine_map()
 
     fig = plt.figure(
-        figsize=(6, 6),
+        figsize=(figure_width, figure_height),
         frameon=False,
-        dpi=400
+        dpi=figure_dpi
     )
 
     # projection=ccrs.LambertConformal(
@@ -43,9 +56,10 @@ def generate_east_asia_plot():
     #     central_latitude=90
     # )
 
+    # 主区域
     ax = add_map_box_main_layout(fig, projection=projection)
 
-    # 添加底图
+    #   添加底图
     add_common_map_feature(
         ax,
         coastline=dict(
@@ -59,7 +73,7 @@ def generate_east_asia_plot():
     for f in nine_features:
         ax.add_feature(f)
 
-    # 坐标轴
+    #   坐标轴
     set_map_box_axis(
         ax,
         xticks=np.arange(70, 141, 10),
@@ -67,14 +81,14 @@ def generate_east_asia_plot():
         projection=projection
     )
 
-    # 网格线
+    #   网格线
     draw_map_box_gridlines(
         ax,
         projection=projection,
         ylocator=[20, 30, 40, 50]
     )
 
-    # 设置区域范围和长宽比
+    #   设置区域范围和长宽比
     set_map_box_area(
         ax,
         area=[70, 140, 15, 55],
@@ -82,9 +96,10 @@ def generate_east_asia_plot():
         aspect=1.25  # 0.75/0.6
     )
 
-    # 九段线子图
+    # 子区域
     sub_ax = add_map_box_sub_layout(fig, projection)
 
+    #   添加地图
     add_common_map_feature(
         sub_ax,
         coastline=dict(
@@ -98,6 +113,7 @@ def generate_east_asia_plot():
     for f in nine_features:
         sub_ax.add_feature(f, linewidth=1)
 
+    #   南海子图
     set_map_box_area(
         sub_ax,
         area=[105, 123, 2, 23],
@@ -105,6 +121,7 @@ def generate_east_asia_plot():
         aspect=0.1 / 0.14
     )
 
+    #   网格线
     draw_map_box_gridlines(
         sub_ax,
         projection=projection,
