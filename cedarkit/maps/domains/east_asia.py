ab802f7be2d9136fe0d2c6e9c9af122464417134
perillaroc <perillaroc@gmail.com>
2024-05-23 20:19:56 +0800

    :art: use map painter in east asia.
    :recycle: update render layer function.

--- cedarkit/maps/domains/east_asia.py
+++ cedarkit/maps/domains/east_asia.py
@@ -1,4 +1,5 @@
-from typing import Union, List, Optional, TYPE_CHECKING
+from typing import Union, List, Optional, Tuple, TYPE_CHECKING
+from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
@@ -25,6 +26,14 @@ from .map_template import MapTemplate
 
 if TYPE_CHECKING:
     from cedarkit.maps.chart import Chart, Panel
+
+
+@dataclass
+class RectSetting:
+    left: float
+    bottom: float
+    width: float
+    height: float
 
 
 class EastAsiaMapTemplate(MapTemplate):
@@ -68,150 +77,23 @@ class EastAsiaMapTemplate(MapTemplate):
         self.main_xticks_interval = 10
         self.main_yticks_interval = 5
 
+        self.sub_xlocator = [110, 120]
+        self.sub_ylocator = [10, 20]
+
+        self.main_map_info_x = 0.998
+        self.main_map_info_y = 0.0022
+        self.main_map_info_text = "Scale 1:20000000 No:GS (2019) 1786"
+
+        self.sub_map_info_x = 0.99
+        self.sub_map_info_y = 0.01
+        self.sub_map_info_text = "Scale 1:40000000"
+
         self.map_loader_class = get_map_loader_class()
 
     def render_panel(self, panel: "Panel"):
         chart = panel.add_chart(domain=self)
         self.load_map()
         self.render_chart(chart=chart)
-
-    def render_chart(self, chart: "Chart"):
-        self.render_main_layer(chart=chart)
-        if self.with_sub_area:
-            self.render_sub_layer(chart=chart)
-
-        rect = draw_map_box(
-            chart.layers[0].ax,
-            bottom_left_point=self.map_box_bottom_left_point,
-            top_right_point=self.map_box_top_right_point,
-        )
-
-    def render_main_layer(self, chart: "Chart") -> Layer:
-        """
-        绘制主地图
-
-        Parameters
-        ----------
-        chart
-
-        Returns
-        -------
-        Layer
-        """
-        fig = chart.fig
-        width = self.width
-        height = self.height
-        layout = [(1 - width) / 2, (1 - height) / 2, width, height]
-        ax = fig.add_axes(
-            layout,
-            projection=self.projection,
-        )
-        layer = Layer(projection=self.projection, chart=chart)
-        layer.set_axes(ax)
-
-        self.main_map_painter.render_layer(layer=layer)
-
-        #   坐标轴
-        # area = self.default_area
-        area = self.area
-        main_xticks = np.arange(
-            area[0],
-            area[1] + self.main_xticks_interval,
-            self.main_xticks_interval
-        )
-        main_yticks = np.arange(
-            area[2],
-            area[3] + self.main_yticks_interval,
-            self.main_yticks_interval
-        )
-        set_map_box_axis(
-            ax,
-            xticks=main_xticks,
-            yticks=main_yticks,
-            projection=self.projection
-        )
-
-        #   网格线
-        # main_ylocator = [20, 30, 40, 50]
-        draw_map_box_gridlines(
-            ax,
-            projection=self.projection,
-            xlocator=main_xticks[1:-1],
-            ylocator=main_yticks[1:-1],
-        )
-
-        #   设置区域范围和长宽比
-        set_map_box_area(
-            ax,
-            area=self.area,
-            projection=self.projection,
-            aspect=self.main_aspect  # 0.75/0.6
-        )
-
-        #   地图信息标注
-        x = 0.998
-        y = 0.0022
-        text = "Scale 1:20000000 No:GS (2019) 1786"
-        add_map_info_text(ax=ax, x=x, y=y, text=text)
-
-        return layer
-
-    def render_sub_layer(self, chart: "Chart") -> Layer:
-        """
-        绘制南海子图
-
-        Parameters
-        ----------
-        chart
-
-        Returns
-        -------
-        Layer
-        """
-        fig = chart.fig
-        main_width = self.width
-        main_height = self.height
-        sub_width = self.sub_width
-        sub_height = self.sub_height
-        ax = fig.add_axes(
-            [(1 - main_width) / 2, (1 - main_height) / 2, sub_width, sub_height],
-            # projection=ccrs.LambertConformal(
-            #     central_longitude=114,
-            #     central_latitude=90,
-            # ),
-            projection=self.projection,
-        )
-        layer = Layer(chart=chart, projection=self.projection)
-        layer.set_axes(ax)
-
-        self.sub_map_painter.render_layer(layer=layer)
-
-        #   区域：南海子图
-        set_map_box_area(
-            ax,
-            area=self.sub_area,
-            projection=self.projection,
-            aspect=self.sub_aspect
-        )
-
-        #   网格线
-        sub_xlocator = [110, 120]
-        sub_ylocator = [10, 20]
-        draw_map_box_gridlines(
-            ax,
-            projection=self.projection,
-            xlocator=sub_xlocator,
-            ylocator=sub_ylocator,
-            linewidth=0.2,
-        )
-
-        #   地图信息标注
-        x = 0.99
-        y = 0.01
-        text = "Scale 1:40000000"
-        add_map_info_text(ax=ax, x=x, y=y, text=text)
-
-        return layer
 
     def load_map(self):
         self.main_map_loader = self.map_loader_class(map_type=MapType.Portrait)
@@ -247,6 +129,7 @@ class EastAsiaMapTemplate(MapTemplate):
             china_rivers_config=MapFeatureConfig(render=True),
             china_nine_lines_config=MapFeatureConfig(render=True),
         )
+
         self.sub_map_painter = MapPainter(
             map_loader=self.sub_map_loader,
             coastline_config=MapFeatureConfig(
@@ -265,6 +148,182 @@ class EastAsiaMapTemplate(MapTemplate):
             china_rivers_config=MapFeatureConfig(render=True),
             china_nine_lines_config=MapFeatureConfig(render=True),
         )
+
+    def render_chart(self, chart: "Chart"):
+        self.render_main_layer(chart=chart)
+        if self.with_sub_area:
+            self.render_sub_layer(chart=chart)
+
+        rect = draw_map_box(
+            chart.layers[0].ax,
+            bottom_left_point=self.map_box_bottom_left_point,
+            top_right_point=self.map_box_top_right_point,
+        )
+
+    def render_main_layer(self, chart: "Chart") -> Layer:
+        """
+        绘制主地图
+
+        Parameters
+        ----------
+        chart
+
+        Returns
+        -------
+        Layer
+        """
+        width = self.width
+        height = self.height
+        rect = RectSetting(
+            left=(1 - width)/2,
+            bottom=(1 - height)/2,
+            width=width,
+            height=height,
+        )
+
+        area = self.area
+        projection = self.projection
+        aspect = self.main_aspect  # 0.75/0.6
+        xticks_interval = self.main_xticks_interval
+        yticks_interval = self.main_yticks_interval
+        map_info_x = self.main_map_info_x
+        map_info_y = self.main_map_info_y
+        map_info_text = self.main_map_info_text
+        map_painter = self.main_map_painter
+
+        # 创建 Layer
+        #       width, height
+        layer = self.create_layer(
+            chart=chart,
+            rect=rect,
+            projection=projection,
+        )
+        ax = layer.ax
+
+        # 设置区域范围和长宽比
+        #       area
+        set_map_box_area(
+            ax,
+            area=area,
+            projection=projection,
+            aspect=aspect
+        )
+
+        # 坐标轴
+        #       area, main_xticks_interval, main_yticks_interval
+        xticks = np.arange(
+            area[0],
+            area[1] + xticks_interval,
+            xticks_interval
+        )
+        yticks = np.arange(
+            area[2],
+            area[3] + yticks_interval,
+            yticks_interval
+        )
+        set_map_box_axis(
+            ax,
+            xticks=xticks,
+            yticks=yticks,
+            projection=projection
+        )
+
+        # 网格线
+        #       同坐标轴
+        draw_map_box_gridlines(
+            ax,
+            projection=projection,
+            xlocator=xticks[1:-1],
+            ylocator=yticks[1:-1],
+        )
+
+        # 地图信息标注
+        #   x, y, text
+        add_map_info_text(
+            ax=ax,
+            x=map_info_x,
+            y=map_info_y,
+            text=map_info_text,
+        )
+
+        #   地图
+        self.plot_map(layer=layer, map_painter=map_painter)
+
+        return layer
+
+    def render_sub_layer(self, chart: "Chart") -> Layer:
+        """
+        绘制南海子图
+
+        Parameters
+        ----------
+        chart
+
+        Returns
+        -------
+        Layer
+        """
+        main_width = self.width
+        main_height = self.height
+        sub_width = self.sub_width
+        sub_height = self.sub_height
+        rect = RectSetting(
+            left=(1 - main_width) / 2,
+            bottom=(1 - main_height) / 2,
+            width=sub_width,
+            height=sub_height,
+        )
+
+        area = self.sub_area
+        projection = self.projection
+        aspect = self.sub_aspect
+        xlocator = self.sub_xlocator
+        ylocator = self.sub_ylocator
+        map_info_x = self.sub_map_info_x
+        map_info_y = self.sub_map_info_y
+        map_info_text = self.sub_map_info_text
+        map_painter = self.sub_map_painter
+
+        # 创建 Layer
+        layer = self.create_layer(
+            chart=chart,
+            rect=rect,
+            projection=self.projection,
+        )
+        ax = layer.ax
+
+        # 区域：南海子图
+        set_map_box_area(
+            ax,
+            area=area,
+            projection=projection,
+            aspect=aspect
+        )
+
+        # 网格线
+        draw_map_box_gridlines(
+            ax,
+            projection=projection,
+            xlocator=xlocator,
+            ylocator=ylocator,
+            linewidth=0.2,
+        )
+
+        # 地图信息标注
+        add_map_info_text(
+            ax=ax,
+            x=map_info_x,
+            y=map_info_y,
+            text=map_info_text,
+        )
+
+        # 地图
+        self.plot_map(layer=layer, map_painter=map_painter)
+
+        return layer
+
+    def plot_map(self, layer: "Layer", map_painter: MapPainter):
+        map_painter.render_layer(layer=layer)
 
     def set_title(
             self,
@@ -363,6 +422,17 @@ class EastAsiaMapTemplate(MapTemplate):
             color_bars.append(color_bar)
 
         return color_bars
+
+    def create_layer(self, chart: "Chart", rect: RectSetting, projection: ccrs.Projection) -> Layer:
+        fig = chart.fig
+        layout = (rect.left, rect.bottom, rect.width, rect.height)
+        ax = fig.add_axes(
+            layout,
+            projection=projection,
+        )
+        layer = Layer(projection=projection, chart=chart)
+        layer.set_axes(ax)
+        return layer
 
 
 class CnAreaMapTemplate(EastAsiaMapTemplate):
